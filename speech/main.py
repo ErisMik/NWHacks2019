f59e9b365a8b059861f92e88433ce7fd6a08b69f
yuming <yumingh7@gmail.com>
2019-01-27 05:55:45 -0800

    changes

--- speech/main.py
+++ speech/main.py
@@ -2,16 +2,19 @@ import redis
 import json
 import configparser
 import urllib.request
+import requests
+from googletrans import Translator
 import azure.cognitiveservices.speech as speechsdk
 
 config = configparser.ConfigParser()
 config.read('speech.config')
+translator = Translator()
 
 speech_key = config['DEFAULT']['SpeechKey']
 speaker_key = config['DEFAULT']['SpeakerKey']
 service_region = config['DEFAULT']['ServiceRegion']
 speech_config = speechsdk.SpeechConfig(subscription=speech_key, region=service_region)
-transl_config = speechsdk.translation.SpeechTranslationConfig(subscription=speech_key, region=service_region, target_languages=['en-ca'], speech_recognition_language='zh-cmn')
+transl_config = speechsdk.translation.SpeechTranslationConfig(subscription=speech_key, region=service_region, target_languages=['en-us'], speech_recognition_language='zh-cn')
 
 speech_recognizer = speechsdk.SpeechRecognizer(speech_config=speech_config)
 transl_recognizer = speechsdk.translation.TranslationRecognizer(translation_config=transl_config)
@@ -21,22 +24,23 @@ r.delete('transcript')
 r.delete('tagged_transcript')
 
 print("Starting speech recognition ...")
+print("Say something (I'm giving up on you).")
 while 1:
-    print("Say something (I'm giving up on you).")
-    result = speech_recognizer.recognize_once()
-    if result.reason == speechsdk.ResultReason.RecognizedSpeech:
-        line = "Recognized: {}".format(result.text)
+    req = requests.get('https://nwhacks-229911.appspot.com/').json()
+    if req['translate'] == 'false':
+        result = speech_recognizer.recognize_once()
+        line = req['speaker'] + ": {}".format(result.text)
         print(line)
         split_line = line.split(":")
         line_dict = {'speaker': split_line[0], 'line': split_line[1]}
         r.rpush('transcript', json.dumps(line_dict))
-
-    elif result.reason == speechsdk.ResultReason.NoMatch:
-        print("No speech could be recognized: {}".format(result.no_match_details))
-    elif result.reason == speechsdk.ResultReason.Canceled:
-        cancellation_details = result.cancellation_details
-        print("Speech Recognition canceled: {}".format(cancellation_details.reason))
-        if cancellation_details.reason == speechsdk.CancellationReason.Error:
-            print("Error details: {}".format(cancellation_details.error_details))
+    else:
+        result = transl_recognizer.recognize_once()
+        trans = translator.translate(result.text)
+        line = req['speaker'] + ": {} (translated from - ".format(trans.text) + result.text + ")"
+        print(line)
+        split_line = line.split(":")
+        line_dict = {'speaker': split_line[0], 'line': split_line[1]}
+        r.rpush('transcript', json.dumps(line_dict))
 
 print("Done!")
