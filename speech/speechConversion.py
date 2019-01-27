0068b36df736e46a21354c524703d504139b6c1c
Harry Yao <harryyao@dhcp-206-87-133-151.ubcsecure.wireless.ubc.ca>
2019-01-26 18:18:38 -0800

    Working speech to text recognition on Azure

---
+++ speech/speechConversion.py
@@ -0,0 +1,20 @@
+import azure.cognitiveservices.speech as speechsdk
+
+speech_key, service_region = "4e8efbac9db7447ba94c6662d1ff2297", "westus"
+speech_config = speechsdk.SpeechConfig(subscription=speech_key, region=service_region)
+
+speech_recognizer = speechsdk.SpeechRecognizer(speech_config=speech_config)
+
+print("Say something...")
+
+result = speech_recognizer.recognize_once()
+
+if result.reason == speechsdk.ResultReason.RecognizedSpeech:
+    print("Recognized: {}".format(result.text))
+elif result.reason == speechsdk.ResultReason.NoMatch:
+    print("No speech could be recognized: {}".format(result.no_match_details))
+elif result.reason == speechsdk.ResultReason.Canceled:
+    cancellation_details = result.cancellation_details
+    print("Speech Recognition canceled: {}".format(cancellation_details.reason))
+    if cancellation_details.reason == speechsdk.CancellationReason.Error:
+        print("Error details: {}".format(cancellation_details.error_details))
