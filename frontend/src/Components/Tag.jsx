e20a4ac8f4e2ae9ef49c47a48738379571784c03
NotMuchToLearn <firepower789@gmail.com>
2019-01-27 05:37:23 -0800

    Front-end almost working

---
+++ frontend/src/Components/Tag.jsx
@@ -0,0 +1,22 @@
+import React from 'react'
+import styled from 'styled-components'
+
+const TagStyle = styled.a`
+	display: inline-block;
+	border-radius: 3px;
+	border: 2px solid white;
+	background: white;
+	color: black;
+`
+
+const Tag = ({tag}) => {
+	return(
+		<div>
+			<TagStyle>
+				{tag}
+			</TagStyle>
+		</div>
+	)
+}
+
+export default Tag
