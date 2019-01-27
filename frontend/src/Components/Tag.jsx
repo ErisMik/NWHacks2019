7f49392650df5999b5f18671fed29f3600e7fed0
NotMuchToLearn <firepower789@gmail.com>
2019-01-27 10:46:11 -0800

    Styled tags

--- frontend/src/Components/Tag.jsx
+++ frontend/src/Components/Tag.jsx
@@ -2,11 +2,12 @@ import React from "react";
 import styled from "styled-components";
 
 const TagStyle = styled.a`
+  margin-bottom: 2px;
   display: inline-block;
   border-radius: 3px;
-  border: 2px solid white;
-  background: white;
-  color: black;
+  border: 2px solid #4292f4;
+  background: #4292f4;
+  color: white;
 `;
 
 const Tag = ({ tag }) => {
