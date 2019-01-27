cd9db83e8b5cd812c00f0743a4eeb4906e34c77c
ajyxc <ajyxcwdlydxwqy@gmail.com>
2019-01-27 12:16:13 -0800

    add action

--- frontend/src/Components/Line.jsx
+++ frontend/src/Components/Line.jsx
@@ -1,22 +1,34 @@
 import React from "react";
 
 export default ({ name, spoke, what, who }) => {
-  return(
-  <div>
+  return (
     <div style={container}>
       <div style={nameStyle}>
-        <p>{name}</p>
+        <p>{name} -</p>
       </div>
 
       <div style={lineStyle}>
-        <p>{spoke}</p>
-        {what ? <img style={jira_icon} src="https://www.teamlead.ru/download/thumbnails/18514301/jira_icon.png?version=1&modificationDate=1301044830000&api=v2"/> : null}
+        <p>- {spoke}</p>
       </div>
-  </div>
-)
+      <div style={image}>
+        {what ? (
+          <img
+            style={jira_icon}
+            src="https://www.teamlead.ru/download/thumbnails/18514301/jira_icon.png?version=1&modificationDate=1301044830000&api=v2"
+          />
+        ) : null}
+      </div>
+    </div>
+  );
 };
-const nameStyle = { color: "#1e8bc3" };
-const lineStyle = { maxwidth: 10, color: "grey", "alignSelf": "flex-end" };
-const lineStyleHighlighted = { maxwidth: 10, color: "grey", "alignSelf": "flex-end", "background": "yellow" };
+const nameStyle = { color: "#c0392b" };
+const lineStyle = { maxwidth: 10, color: "grey", alignSelf: "flex-end" };
+const lineStyleHighlighted = {
+  maxwidth: 10,
+  color: "grey",
+  alignSelf: "flex-end",
+  background: "yellow"
+};
 const container = { display: "flex" };
-const jira_icon = { width: "30px", height: "30px", display: "inline"};
+const jira_icon = { width: "30px", height: "30px", display: "inline" };
+const image = {};
